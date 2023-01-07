20feb2c697be9cf9999a641ecab46a184bee6d50
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2023-01-07 02:57:30 +0100

    Add grabber mechanic

---
+++ SpaceScrapper/Assets/_Assets/_Scripts/Runtime/Player/MovablePart.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wokarol.SpaceScrapper.Player
+{
+    public class MovablePart : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D body = null;
+        [SerializeField] private List<Vector2> forwardAxes = new List<Vector2>() { Vector2.up };
+        private Vector2 smoothDampVelocity;
+        private float smoothDampAngleVelocity;
+
+        Vector2 forwardAxis = Vector2.zero;
+
+        public void MoveTowards(Vector3 position, Vector2 direction)
+        {
+            body.position = Vector2.SmoothDamp(body.position, position, ref smoothDampVelocity, 0.05f);
+
+            Debug.DrawRay(transform.position, forwardAxis * 2f);
+            Debug.DrawRay(transform.position, direction * 2f);
+
+            var newAngle = Vector2.SignedAngle(forwardAxis, direction);
+
+            body.rotation = Mathf.SmoothDampAngle(body.rotation, newAngle, ref smoothDampAngleVelocity, 0.05f);
+        }
+
+        public void StartMove(Vector2 initialDirection)
+        {
+            body.isKinematic = true;
+            body.velocity = Vector3.zero;
+            body.angularVelocity = 0;
+
+            float closestDot = -1;
+            Vector2 closestDirection = Vector2.zero;
+            for (int i = 0; i < forwardAxes.Count; i++)
+            {
+                float dot = Vector2.Dot(transform.TransformDirection(forwardAxes[i].normalized), initialDirection.normalized);
+                
+                if (dot > closestDot)
+                {
+                    closestDot = dot;
+                    closestDirection = forwardAxes[i].normalized;
+                }
+            }
+
+            if (closestDirection == Vector2.zero)
+            {
+                Debug.LogWarning("Could not find any direction while starting the move", this);
+            }
+
+            forwardAxis = closestDirection;
+        }
+
+        public void StopMove()
+        {
+            body.isKinematic = false;
+            body.velocity = smoothDampVelocity;
+            body.angularVelocity = smoothDampAngleVelocity;
+        }
+    }
+}
--- SpaceScrapper/Assets/_Assets/_Scripts/Runtime/Player/Player.cs
+++ SpaceScrapper/Assets/_Assets/_Scripts/Runtime/Player/Player.cs
@@ -1,6 +1,7 @@
 using Cinemachine;
 using System;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Wokarol.Common;
@@ -16,9 +17,12 @@ namespace Wokarol.SpaceScrapper.Player
         [SerializeField] private Transform aimPoint = null;
         [SerializeField] private Animator animator = null;
         [SerializeField] private CinemachineVirtualCameraBase closeUpCamera = null;
+        [SerializeField] private Collider2D grabbingTrigger = null;
+        [SerializeField] private Transform grabTarget = null;
         [Header("Parameters")]
         [SerializeField] private float maxAimDistance = 3;
         [SerializeField] private ShipMovementParams movement = new();
+        [SerializeField] private ShipMovementParams movementWhenHolding = new();
         [Header("Axis")]
         [SerializeField] private Vector2 forwardAxis = Vector2.up;
         [SerializeField] private Vector2 thrusterForwardAxis = Vector2.up;
@@ -28,7 +32,9 @@ namespace Wokarol.SpaceScrapper.Player
         [SerializeField] private float thrusterNoiseSpeed = 5f;
         [Header("Animations")]
         [SerializeField] private AnimationNames animatorKeys = new();
-        
+        [Header("Grabbing")]
+        [SerializeField] private LayerMask grabbableLayerMask = 0;
+
 
         private SceneContext sceneContext;
         private PlayerInputActions input;
@@ -36,14 +42,17 @@ namespace Wokarol.SpaceScrapper.Player
         private InputValues lastInputValues;
         private MovementValues lastMovementValues;
 
+        private InteractionState interactionState = InteractionState.Idle;
+        private MovablePart grabbedPart = null;
+
+        private List<Collider2D> colliderListCache = new();
+
+
         private void Start()
         {
             sceneContext = GameSystems.Get<SceneContext>();
 
-            input = new PlayerInputActions();
-            playerInput.user.AssociateActionsWithUser(input);
-
-            input.Enable();
+            SetupInput();
         }
 
         private void Update()
@@ -52,21 +61,28 @@ namespace Wokarol.SpaceScrapper.Player
             lastInputValues = HandleInput(mainCamera);
             PositionAimPoint(lastInputValues);
             UpdateThrusterAnimation(lastMovementValues);
-
-            if (Keyboard.current.pKey.wasPressedThisFrame)
-            {
-                animator.CrossFade(animatorKeys.OpenWingsState, 0.2f);
-            }
-
-            if (Keyboard.current.oKey.wasPressedThisFrame)
-            {
-                animator.CrossFade(animatorKeys.CloseWingsState, 0.2f);
-            }
         }
 
         private void FixedUpdate()
         {
             lastMovementValues = HandleMovement(lastInputValues);
+
+            if (interactionState == InteractionState.HoldingPart)
+            {
+                grabbedPart.MoveTowards(grabTarget.position, grabTarget.up);
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (input != null)
+                input.Enable();
+        }
+
+        private void OnDisable()
+        {
+            if (input != null)
+                input.Disable();
         }
 
         public void EnableCloseUpCamera()
@@ -79,6 +95,15 @@ namespace Wokarol.SpaceScrapper.Player
         {
             if (closeUpCamera != null)
                 closeUpCamera.enabled = false;
+        }
+
+        private void SetupInput()
+        {
+            input = new PlayerInputActions();
+            playerInput.user.AssociateActionsWithUser(input);
+
+            input.Enable();
+            input.Flying.Grab.performed += Grab_performed;
         }
 
         private void UpdateThrusterAnimation(MovementValues values)
@@ -126,13 +151,66 @@ namespace Wokarol.SpaceScrapper.Player
             return new InputValues(thrust, direction, aimPointInWorldSpace);
         }
 
+        private void Grab_performed(InputAction.CallbackContext ctx)
+        {
+            bool isPressed = ctx.ReadValueAsButton();
+
+            switch (interactionState)
+            {
+                case InteractionState.Idle when isPressed:
+                    TryToGrabPart();
+                    break;
+
+                case InteractionState.HoldingPart when !isPressed:
+                    SwitchState(InteractionState.Idle);
+                    break;
+            }
+        }
+
+        private void TryToGrabPart()
+        {
+            grabbingTrigger.OverlapCollider(new ContactFilter2D()
+            {
+                layerMask = grabbableLayerMask,
+                useLayerMask = true,
+            }, colliderListCache);
+
+            float closestDistance = float.PositiveInfinity;
+            MovablePart closestPart = null;
+
+            foreach (var collider in colliderListCache)
+            {
+                if (collider.TryGetComponent(out MovablePart part))
+                {
+                    float distance = Vector2.Distance(grabTarget.position, part.transform.position);
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        closestPart = part;
+                    }
+                }
+            }
+
+            if (closestPart != null)
+            {
+                grabbedPart = closestPart;
+                SwitchState(InteractionState.HoldingPart);
+            }
+            else
+            {
+                animator.CrossFade(animatorKeys.OpenWingsFailState, 0.1f);
+            }
+        }
+
         private MovementValues HandleMovement(InputValues values)
         {
             var movementValues = new MovementValues();
+            var movementParams = interactionState == InteractionState.HoldingPart ? movementWhenHolding : movement;
+
             if (values.Thrust.magnitude > 0)
             {
                 Vector2 thrustPower = values.Thrust;
-                body.AddForce(movement.Thrust * thrustPower);
+                body.AddForce(movementParams.Thrust * thrustPower);
 
                 movementValues.ThrustVector = thrustPower;
             }
@@ -142,7 +220,7 @@ namespace Wokarol.SpaceScrapper.Player
             float currentRotation = body.rotation;
             float angVelocity = body.angularVelocity;
 
-            float newRotation = Mathf.SmoothDampAngle(currentRotation, targetRotation, ref angVelocity, movement.RotationSmoothing);
+            float newRotation = Mathf.SmoothDampAngle(currentRotation, targetRotation, ref angVelocity, movementParams.RotationSmoothing);
 
             body.angularVelocity = angVelocity;
             body.rotation = newRotation;
@@ -150,8 +228,43 @@ namespace Wokarol.SpaceScrapper.Player
             return movementValues;
         }
 
+        private void SwitchState(InteractionState newState)
+        {
+            var lastState = interactionState;
+            interactionState = newState;
 
-        readonly struct InputValues
+            if (lastState == interactionState)
+                return;
+
+            if (lastState == InteractionState.HoldingPart)
+            {
+                if (grabbedPart != null)
+                {
+                    grabbedPart.StopMove();
+                    grabbedPart = null;
+                }
+
+                animator.CrossFade(animatorKeys.CloseWingsState, 0.2f);
+            }
+
+            if (newState == InteractionState.HoldingPart)
+            {
+                animator.CrossFade(animatorKeys.OpenWingsState, 0.2f);
+
+                if (grabbedPart == null)
+                    Debug.LogWarning($"You should not enter the {nameof(InteractionState.HoldingPart)} state without setting {grabbedPart}", this);
+                else
+                    grabbedPart.StartMove(grabTarget.up);
+            }
+        }
+
+        private enum InteractionState
+        {
+            Idle,
+            HoldingPart,
+        }
+
+        private readonly struct InputValues
         {
             public readonly Vector2 Thrust;
             public readonly Vector2 AimDirection;
@@ -167,20 +280,21 @@ namespace Wokarol.SpaceScrapper.Player
             public static InputValues Empty => new(Vector2.zero, Vector2.up, Vector2.up);
         }
 
-        struct MovementValues
+        private struct MovementValues
         {
             public Vector2 ThrustVector;
         }
 
         [System.Serializable]
-        class AnimationNames
+        private class AnimationNames
         {
             [field: SerializeField] public string OpenWingsState { get; private set; } = "Open-Wings";
+            [field: SerializeField] public string OpenWingsFailState { get; private set; } = "Open-Wings-Fail";
             [field: SerializeField] public string CloseWingsState { get; private set; } = "Close-Wings";
         }
 
         [System.Serializable]
-        class ShipMovementParams
+        private class ShipMovementParams
         {
             public float Thrust = 60;
             public float RotationSmoothing = 0.2f;
