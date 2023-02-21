4904b406e32353c8ba44a6cb57f1c4029030dfdb
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2023-02-21 12:14:33 +0100

    Add health to the core and make it hittable

--- SpaceScrapper/Assets/_Assets/_Scripts/Runtime/Actors/Buildings/BaseCore.cs
+++ SpaceScrapper/Assets/_Assets/_Scripts/Runtime/Actors/Buildings/BaseCore.cs
@@ -1,11 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Wokarol.SpaceScrapper.Weaponry;
 
 namespace Wokarol.SpaceScrapper
 {
-    public class BaseCore : MonoBehaviour
+    public class BaseCore : MonoBehaviour, IHittable
     {
+        [SerializeField] private int maxHealth = 200;
 
+        private int health;
+
+        public int Health => health;
+        public event Action Destoyed;
+
+        private void Awake()
+        {
+            health = maxHealth;
+        }
+
+        public void Hit(Vector2 force, Vector2 normal, Vector2 point, int damage)
+        {
+            if (damage <= 0) return;
+
+            health -= damage;
+
+            if (health <= 0)
+            {
+                Destoyed?.Invoke();
+                Destroy(gameObject);
+            }
+        }
     }
 }
