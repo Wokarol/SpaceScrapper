60db9b040d61b9ab195366246ad201510c43afa6
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2023-02-22 21:42:16 +0100

    Add check for the current state of the scenes to Scene Director

--- SpaceScrapper/Assets/_Assets/_Scripts/Runtime/Global/SceneDirector.cs
+++ SpaceScrapper/Assets/_Assets/_Scripts/Runtime/Global/SceneDirector.cs
@@ -12,6 +12,48 @@ namespace Wokarol.SpaceScrapper.Global
         [SerializeField, Scene] private string hubScene = "";
         [SerializeField, Scene] private string menuScene = "";
 
+        private void Start()
+        {
+            HashSet<string> loadedScenes = new();
+            for (int i = 0; i < SceneManager.sceneCount; i++)
+            {
+                loadedScenes.Add(SceneManager.GetSceneAt(i).name);
+            }
+
+            if (loadedScenes.Contains(menuScene))
+            {
+                Debug.Log("<color=cyan>Scene Director:</color> Started in the menu");
+            }
+
+            bool anyGameplaySceneIsLoaded = false;
+            bool allScenesAreLoaded = true;
+
+            for (int i = 0; i < gameplayScenes.Count; i++)
+            {
+                if (loadedScenes.Contains(gameplayScenes[i]))
+                    anyGameplaySceneIsLoaded = true;
+                else
+                    allScenesAreLoaded = false;
+            }
+
+            if (anyGameplaySceneIsLoaded && !allScenesAreLoaded)
+            {
+                if (allScenesAreLoaded)
+                {
+                    Debug.Log("<color=cyan>Scene Director:</color> Started with the gameplay scenes");
+                }
+                else
+                {
+                    Debug.Log("<color=cyan>Scene Director:</color> Started with the gameplay scenes, not all scenes are loaded");
+                }
+            }
+
+            if (loadedScenes.Contains(hubScene))
+            {
+                Debug.Log("<color=cyan>Scene Director:</color> Started in the Hub");
+            }
+        }
+
         public void RemoveAllAndLoadGameplayScenes()
         {
             for (int i = 0; i < gameplayScenes.Count; i++)
