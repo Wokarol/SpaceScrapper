7b64349a7208faa35f9a44f2df2309813b949dfa
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2023-02-28 22:10:57 +0100

    Analyze the old targeting method, prepare comments for further refactor

--- SpaceScrapper/Assets/_Assets/_Scripts/Runtime/Combat/TargetingManager.cs
+++ SpaceScrapper/Assets/_Assets/_Scripts/Runtime/Combat/TargetingManager.cs
@@ -13,6 +13,13 @@ namespace Wokarol.SpaceScrapper.Combat
 
         public CombatActor GetBestTargetToFight(CombatActor caller, float? maxSeeingDistance = null)
         {
+            // TODO: Redesign the algorithm
+            // 1. Remove Linq in favour of manual looping
+            // 2. Make priority a weight instead of hard sort (distance * a  +  priority * b  +  ...)
+            // 3. Consider adding checks to see if the new target is needed in a guard clause-like fashion (if target exists and is rather close, return it instead of searching more)
+
+            // TODO: Check performance of that algorithm (stress test and stuff)
+
             // TODO: Optimize this mess later
             IEnumerable<(CombatActor a, float d)> fittingTargets = actors
                 .Where(a => a.Faction != caller.Faction)
@@ -22,12 +29,13 @@ namespace Wokarol.SpaceScrapper.Combat
 
             if (maxSeeingDistance != null)
             {
+                // That should probably be done first to limit the search space
                 fittingTargets = fittingTargets
                     .Where(ad => ad.d < maxSeeingDistance);
             }
 
             return fittingTargets // TODO: Improve target choice algorithm
-                .FirstOrDefault().a;
+                .FirstOrDefault().a; // The f..ck is "a"
         }
 
         public void AddActor(CombatActor combatActor)
