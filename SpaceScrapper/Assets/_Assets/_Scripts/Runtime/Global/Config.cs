ef87d35c82cecff7f33e03dae7fd82be109a6cbb
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2023-03-08 14:48:46 +0100

    Update Game System Locator package

--- SpaceScrapper/Assets/_Assets/_Scripts/Runtime/Global/Config.cs
+++ SpaceScrapper/Assets/_Assets/_Scripts/Runtime/Global/Config.cs
@@ -1,14 +1,14 @@
 using Wokarol.Common;
-using Wokarol.GameSystemsLocator;
+using Wokarol.GameSystemsLocator.Bootstrapping;
+using Wokarol.GameSystemsLocator.Core;
 using Wokarol.SpaceScrapper.Combat;
 using Wokarol.SpaceScrapper.Global;
 
 namespace Wokarol.SpaceScrapper
 {
-
     public class GameConfiguration : ISystemConfiguration
     {
-        public void Configure(GameSystems.ConfigurationBuilder builder)
+        public void Configure(ServiceLocatorBuilder builder)
         {
             builder.PrefabPath = "Systems";
 
