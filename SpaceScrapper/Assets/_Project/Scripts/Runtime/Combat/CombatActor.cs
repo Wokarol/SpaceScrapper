5646016fcf380764da96c86ad287c4ac3bddb79c
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2023-03-27 23:13:47 +0200

    Remove register code from Combat Actor and replace it with simpler approach

--- SpaceScrapper/Assets/_Project/Scripts/Runtime/Combat/CombatActor.cs
+++ SpaceScrapper/Assets/_Project/Scripts/Runtime/Combat/CombatActor.cs
@@ -8,47 +8,17 @@ namespace Wokarol.SpaceScrapper.Combat
         [SerializeField] private Faction faction;
         [SerializeField, Tooltip("Bigger = More important")] private int priority = 0;
 
-        TargetingManager manager;
-        bool isAdded = false;
-        bool startPassed = false;
-
         public Faction Faction => faction;
         public int Priority => priority;
 
-        // REFACTOR: Reconsider the register approach
-        private void Start()
-        {
-            startPassed = true;
-            manager = GameSystems.Get<TargetingManager>();
-
-            RegisterMyself();
-        }
-
         private void OnEnable()
         {
-            if (startPassed && !isAdded) RegisterMyself();
+            GameSystems.Get<TargetingManager>().AddActor(this);
         }
 
         private void OnDisable()
         {
-            if (startPassed && isAdded) RemoveMyself();
-        }
-
-        private void OnDestroy()
-        {
-            if (startPassed && isAdded) RemoveMyself();
-        }
-
-        private void RegisterMyself()
-        {
-            isAdded = true;
-            manager.AddActor(this);
-        }
-
-        private void RemoveMyself()
-        {
-            isAdded = false;
-            manager.RemoveActor(this);
+            GameSystems.Get<TargetingManager>().RemoveActor(this);
         }
     }
 }
